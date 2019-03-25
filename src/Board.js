7a87d7a84af7138e74f375d4c48bce34135b4c64
jesmur <jessicamurray91@gmail.com>
2019-03-25 16:28:50 -0300

    chapter 5_3

--- src/Board.js
+++ src/Board.js
@@ -1,31 +1,36 @@
 import React, { Component } from 'react'
 import Note from './Note'
+import { FaPlus } from 'react-icons/fa'
 
 class Board extends Component {
     constructor(props){
         super(props)
         this.state = {
-            notes: [
-                {
-                    id: 0,
-                    note: "Call Devin"
-                },
-                {
-                    id: 1,
-                    note: "E-mail Chris"
-                },
-                {
-                    id: 2,
-                    note: "Order pizza"
-                }
-            ]
+            notes: []
         }
         this.eachNote = this.eachNote.bind(this)
         this.update = this.update.bind(this)
         this.remove = this.remove.bind(this)
+        this.add = this.add.bind(this)
+        this.nextId = this.nextId.bind(this)
     }
 
-    
+    add(text){
+        this.setState(prevState => ({
+            notes: [
+                ...prevState.notes,
+                {
+                    id: this.nextId(),
+                    note: text
+                }
+            ]
+        }))
+    }
+
+    nextId(){
+        this.uniqueId = this.uniqueId || 0;
+        return this.uniqueId++;
+    }
 
     update(newText, i){
         console.log('updating item at index', i, newText)
@@ -58,6 +63,10 @@ class Board extends Component {
         return(
             <div className="board">
                 {this.state.notes.map(this.eachNote)}
+                <button id="add" 
+                    onClick={this.add.bind(null, "New Note")}>
+                    <FaPlus />
+                </button>
             </div>
         )
     }
